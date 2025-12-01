aa65e10ebd9e441e06918ee5654efdb52f005b91
andreibel <andreinw12@gmail.com>
2025-12-01 19:19:40 +0200

    initialize Spring Boot project with Telegram bot integration and necessary configurations

---
+++ src/main/java/com/andreibel/springbot/SpringBotApplication.java
@@ -0,0 +1,13 @@
+package com.andreibel.springbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBotApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SpringBotApplication.class, args);
+    }
+
+}
