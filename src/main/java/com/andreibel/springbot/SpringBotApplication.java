f7c0133eadc7ccad7f4fe876133882521831011f
andreibel <andreinw12@gmail.com>
2025-12-27 00:09:45 +0200

    remove inappropriate comment from SpringBotApplication.java
    haha, all the "lacked" keys in that repo are not valid now you cannot even try

--- src/main/java/com/andreibel/springbot/SpringBotApplication.java
+++ src/main/java/com/andreibel/springbot/SpringBotApplication.java
@@ -3,10 +3,11 @@ package com.andreibel.springbot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// haha, all the "lacked" keys in that repo are not valid now you cannot even try
 @SpringBootApplication
 public class SpringBotApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(SpringBotApplication.class, args);
     }
 
